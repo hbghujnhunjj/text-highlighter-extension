7ea08012f42eace614107f64f16d8da419f9c21d
Zoe Gorbachev <113218792+hbghujnhunjj@users.noreply.github.com>
2023-07-05 12:18:21 -0400

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,11 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'highlightSaved') {
     sendResponse({ message: 'Highlight saved successfully!' });
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'icon.png',
+      title: 'Text Highlighter',
+      message: 'The highlighted text has been saved.'
+    });
   }
 });
